bd3dee4b7d9aee736a5f79195cd600f44ec2291a
ZackQ <18207143503@163.com>
2018-11-16 11:02:42 +0800

    测试用例

--- 算法竞赛入门经典/C++与STL入门/集合栈计算机.cpp
+++ 算法竞赛入门经典/C++与STL入门/集合栈计算机.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <algorithm>
 #include <stack>
+#include <set>
 #include <cassert>
 using namespace std;
 
@@ -10,7 +11,7 @@ using namespace std;
 
 class setStack {
 private:
-	stack<set<string>> stk;
+	stack<multiset<string>> stk;
 public:
 	setStack() = default;
 	void Push();
@@ -19,17 +20,19 @@ public:
 	void Intersect();
 	void Add();
 	size_t Length() const;
+	// 打印栈中的所有集合
+	void PrintStack();
 };
 
 void setStack::Push() {
 	// push直接放入空栈即可
-	stk.push(set<string> ());
+	stk.push(multiset<string> ());
 }
 
 void setStack::Dup() {
 	// dup操作则是复制栈顶元素，然后放入栈中
 	assert(!stk.empty());
-	auto t = dup.top();
+	auto t = stk.top();
 	stk.push(t);
 }
 
@@ -69,31 +72,62 @@ void setStack::Intersect() {
 		}
 	} else {
 		// 求交集
-		set<string> res;
+		multiset<string> res;
 		for (auto &var : s1) {
 			if (s2.find(var) != s2.end()) {
-				res.push(var);
+				res.insert(var);
 			}
 		}
 		stk.push(res);
 	}
 }
 
-void setStack::add() {
+void setStack::Add() {
 	// 添加操作
 	assert(stk.size() >= 2);
 	auto s1 = stk.top(); stk.pop();
 	auto s2 = stk.top(); stk.pop();
 	string s;
 	for (auto it = s1.begin(); it != s1.end(); it++) {
-		s.append(*it).append(",");
+		s.append(*it);
+		s.append(",");
 	}
 	if (!s.empty()) {
 		s.pop_back();
 	}
-	s2.push("{" + s + "}");
+	s2.insert("{" + s + "}");
 	stk.push(s2);
 
+}
+
+size_t setStack::Length() const {
+	return stk.size();
+}
+
+// 打印栈元素的思路，采用了递归的思路去做
+// 从栈顶部开始打印
+void setStack::PrintStack(){
+	if (stk.empty()) {
+		return;
+	} else {
+		auto s = stk.top(); stk.pop();
+		cout << "{";
+		string line;
+		for (auto &var : s) {
+			line.append(var);
+			line.append(",");
+		}
+		// 去掉最后一个逗号
+		if (!line.empty()) {
+			line.pop_back();
+		}
+		cout << line;
+		cout << "}" << endl;
+		//接下来打印其他元素
+		PrintStack();
+		// 恢复原来的栈的内容
+		stk.push(s);
+	}
 }
 
 int main(void) {
@@ -101,6 +135,63 @@ int main(void) {
 	setStack stk;
 	string ops;
 	while (cin >> ops) {
-
+		if (ops[0] == 'P') {
+			stk.Push();
+		} else if (ops[0] == 'D') {
+			stk.Dup();
+		} else {
+			if (ops[0] == 'U') stk.Union();
+			if (ops[0] == 'I') stk.Intersect();
+			if (ops[0] == 'A') stk.Add();
+		}
+		stk.PrintStack();
+		cout << endl << endl;
 	}
 }
+
+/**
+ * 测试用例
+ * PUSH
+ * DUP
+ * ADD
+ * PUSH
+ * ADD
+ * DUP
+ * ADD
+ * DUP
+ * UNION
+ */
+
+// 对应的输出
+
+/**
+{}              
+                
+                
+{}              
+{}              
+                
+                
+{{}}            
+                
+                
+{}              
+{{}}            
+                
+                
+{{},{}}         
+                
+                
+{{},{}}         
+{{},{}}         
+                
+                
+{{{},{}},{},{}} 
+                
+                
+{{{},{}},{},{}} 
+{{{},{}},{},{}} 
+                
+                
+{{{},{}},{},{}} 
+**/
