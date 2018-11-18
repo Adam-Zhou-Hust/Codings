fb7e73e8a3861fcd1735cac5bcd347c8246aac46
ZackQ <18207143503@163.com>
2018-11-18 19:09:00 +0800

    队列的应用

---
+++ 算法竞赛入门经典/C++与STL入门/团体队列.cpp
@@ -0,0 +1,16 @@
+// 三种操作
+// 编号x的人，进入长队
+// 长队的队首出队伍，并给出出队伍的人的编号
+// 停止模拟
+
+// 一共有t个团队的人
+// 实际上是一个队列模拟题
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <queue>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
